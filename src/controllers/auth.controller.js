be10ce2be3000a2b387bb5101cb3401cbadcdcaf
giovanyosorio <giovany.torres.799@gmail.com>
2023-06-25 20:46:45 -0500

    Creación del Login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import {createAccesToken  } from "../libs/jwt.js";
+
+
+
 export const register = async (req, res) => {
     const { email, password, username } = req.body
     try {
@@ -32,7 +35,46 @@ export const register = async (req, res) => {
 
 }
 
-export const login = (req, res) => {
-    res.send("login")
+export const login = async (req, res) => {
+
     console.log("login");
+    const { email, password } = req.body
+    try {
+       
+        const userFound=await User.findOne({email})
+
+        if(!userFound){
+         return   res.status(400).json({message:"user not found"})
+        }
+        const isMatch = await bcryptjs.compare(password, userFound.password)
+
+        if (!isMatch) {
+         return    res.status(400).json({message:"incorrect password"})
+        }
+
+
+        const token= await createAccesToken({id:userFound._id})
+        res.cookie("token",token)
+ /*        res.json({
+            message:"usuario creado "
+        }) */
+         res.json({
+            id: userFound._id,
+            username: userFound.username,
+            email: userFound.email,
+            createdAt: userFound.createdAt,
+            updatedAt: userFound.updatedAt
+        }) 
+    } catch (error) {
+        res.status(500).json({
+            message:error.message
+        })
+    }
+}
+
+export const logout= (req,res)=>{
+    res.cookie("token","",{
+        expires:new Date(0)
+    })
+    return res.sendStatus(200)
 }
