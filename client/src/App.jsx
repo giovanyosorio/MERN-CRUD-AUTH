14780f5035f50ad4d0c6771ca0a6c316f539857f
giovanyosorio <giovany.torres.799@gmail.com>
2023-06-27 20:39:57 -0500

    frontend con react tailwind-Configuración del Cliente

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/login" element={<h1>Login</h1>} />
+        <Route path="/Register" element={<h1>Register</h1>} />
+        <Route path="/tasks" element={<h1>tasks</h1>} />
+        <Route path="/add-task" element={<h1>New task</h1>} />
+        <Route path="/tasks/:id" element={<h1>update task</h1>} />
+        <Route path="/profile" element={<h1>profile</h1>} />
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
